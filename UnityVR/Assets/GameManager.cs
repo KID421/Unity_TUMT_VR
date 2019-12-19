37a48c879c4de463fe25b0e4b50c6aea60d0ff5f
KID <50666207+KID421@users.noreply.github.com>
2019-12-19 10:57:41 +0800

    完成並發布測試

---
+++ UnityVR/Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool haveKey;
+
+    // 公開並且參數零或一個
+    public void ThrowPickUp()
+    {
+        print("撿起物件");
+    }
+
+    public void ThrowDetach(GameObject obj)
+    {
+        print("放開物件");
+        haveKey = true;
+        Destroy(obj);
+    }
+
+    public void ThrowHeld()
+    {
+        print("拿著物件");
+    }
+}
